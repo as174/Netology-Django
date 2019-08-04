742205644c5bc98b65d29e54da8ca12390fc8a45
as174 <42006639+as174@users.noreply.github.com>
2019-08-04 19:15:07 +0300

    Update views.py

--- request-handling/pagination/app/views.py
+++ request-handling/pagination/app/views.py
@@ -1,16 +1,33 @@
 from django.shortcuts import render_to_response, redirect
 from django.urls import reverse
-
+import csv
+import urllib
+from app.settings import BUS_STATION_CSV
 
 def index(request):
     return redirect(reverse(bus_stations))
 
 
 def bus_stations(request):
+    page = int(request.GET.get('page', 1))
+    bus_stations_list = []
+    with open(BUS_STATION_CSV, encoding='cp1251') as csvfile:
+        stations_list = list(csv.DictReader(csvfile))
+        for i in range((page - 1)*10, page*10 - 1):
+            station_dict = {}
+            station_dict['Name'] = stations_list[i]['Name']
+            station_dict['Street'] = stations_list[i]['Street']
+            station_dict['District'] = stations_list[i]['District']
+            bus_stations_list.append(station_dict)
+    next_page_url = reverse('bus_stations') + '?' + urllib.parse.urlencode({'page': page + 1})
+    if page > 1:
+        args = {'page': page - 1}
+        prev_page_url = reverse('bus_stations') + '?' + urllib.parse.urlencode(args)
+    else: 
+        prev_page_url = None
     return render_to_response('index.html', context={
-        'bus_stations': [{'Name': 'название', 'Street': 'улица', 'District': 'район'}],
-        'current_page': 1,
-        'prev_page_url': None,
-        'next_page_url': 'bus_stations/?page=2',
-    })
-
+    	'bus_stations': bus_stations_list, 
+    	'current_page': page,
+    	'prev_page_url': prev_page_url,
+    	'next_page_url': next_page_url
+    	})
